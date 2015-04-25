7e3cc6d92f0296410cb83d0d41c235d7e456ad0e
Erika Cho <cho.erika@lib-gate.co.jp>
2015-04-25 20:26:56 +0900

    http通信をApiFactory内にまとめ。

---
+++ src/static/app/common/factorys/ApiFactory.js
@@ -0,0 +1,76 @@
+angular.module('dbflute-intro').factory('ApiFactory',
+        function ($rootScope, $http) {
+    'use strict';
+
+    var errorHandler = function(data, status) {
+
+    };
+
+    return {
+        manifest: function() {
+            return $http({
+                method : 'POST',
+                url : 'api/intro/manifest'
+          }).success(function(data) {
+          }).error(errorHandler);
+        },
+        publicProperties: function() {
+            return $http({
+                method : 'POST',
+                url : 'api/engine/publicProperties'
+          }).success(function(data) {
+          }).error(errorHandler);
+        },
+        engineVersions: function() {
+            return $http({
+                method : 'POST',
+                url : 'api/engine/versions'
+          }).success(function(data) {
+          }).error(errorHandler);
+        },
+        classification: function() {
+            return $http({
+                method : 'POST',
+                url : 'api/client/classification'
+          }).success(function(data) {
+          }).error(errorHandler);
+        },
+        clientBeanList: function() {
+            return $http({
+                method : 'POST',
+                url : 'api/client/list'
+          }).success(function(data) {
+          }).error(errorHandler);
+        },
+        clientCreate: function(params) {
+            return $http({
+                method : 'POST',
+                url : 'api/client/create',
+                data : {clientBean: params}
+          }).success(function(data) {
+          }).error(errorHandler);
+        },
+        clientUpdate: function(params) {
+            return $http({
+                method : 'POST',
+                url : 'api/client/update',
+                data : {clientBean: params}
+          }).success(function(data) {
+          }).error(errorHandler);
+        },
+        clientRemove: function(params) {
+            return $http({
+                method : 'POST',
+                url : 'api/client/remove' + params.project
+          }).success(function(data) {
+          }).error(errorHandler);
+        },
+        downloadEngine: function(params) {
+            return $http({
+                method : 'POST',
+                url : 'api/engine/download/' + params.version
+          }).success(function(data) {
+          }).error(errorHandler);
+        }
+    };
+});
--- src/static/app/main/main.controller.js
+++ src/static/app/main/main.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('dbflute-intro')
-  .controller('MainCtrl', function ($scope, $http, $window) {
+  .controller('MainCtrl', function ($scope, $http, $window, ApiFactory) {
 
       $scope.manifest = {};
       $scope.publicProperties = [];
@@ -12,128 +12,99 @@ angular.module('dbflute-intro')
       $scope.editFlg = false;
 
       $scope.manifest = function() {
-          $http({
-              method : 'POST',
-              url : 'api/intro/manifest'
-          }).success(function(data) {
-              $scope.manifest = data;
+          ApiFactory.manifest().then(function(response) {
+              $scope.manifest = response.data;
           });
-      }
+      };
 
       $scope.publicProperties = function(version) {
-          $http({
-              method : 'POST',
-              url : 'api/engine/publicProperties'
-          }).success(function(data) {
-              $scope.publicProperties = data;
+          ApiFactory.publicProperties().then(function(response) {
+              $scope.publicProperties = response.data;
           });
-      }
+      };
 
       $scope.engineVersions = function(version) {
-          $http({
-              method : 'POST',
-              url : 'api/engine/versions'
-          }).success(function(data) {
-              $scope.versions = data;
+          ApiFactory.engineVersions().then(function(response) {
+              $scope.versions = response.data;
           });
-      }
-
-      $scope.engineVersions = function(version) {
-          $http({
-              method : 'POST',
-              url : 'api/engine/versions'
-          }).success(function(data) {
-              $scope.versions = data;
-          });
-      }
+      };
 
       $scope.classification = function() {
-          $http({
-              method : 'POST',
-              url : 'api/client/classification'
-          }).success(function(data) {
-              $scope.classificationMap = data;
+          ApiFactory.classification().then(function(response) {
+              $scope.classificationMap = response.data;
           });
-      }
+      };
 
-      $scope.list = function() {
-          $http({
-              method : 'POST',
-              url : 'api/client/list'
-          }).success(function(data) {
-              $scope.clientBeanList = data;
+      $scope.clientBeanList = function() {
+          ApiFactory.clientBeanList().then(function(response) {
+              $scope.clientBeanList = response.data;
           });
-      }
+      };
 
       $scope.add = function() {
           $scope.editFlg = true;
           $scope.clientBean = {};
-      }
+      };
 
       $scope.edit = function() {
           $scope.editFlg = true;
-      }
+      };
 
       $scope.cancelEdit = function() {
           $scope.editFlg = false;
-      }
+      };
 
       $scope.create = function(clientBean) {
-          $http({
-              method : 'POST',
-              url : 'api/client/create',
-              data : {clientBean: clientBean}
-          }).success(function(data) {
-              $scope.list();
+          ApiFactory.clientCreate(clientBean).then(function(response) {
+              $scope.clientBeanList();
           });
-      }
+      };
 
       $scope.update = function(clientBean) {
-          $http({
-              method : 'POST',
-              url : 'api/client/update',
-              data : {clientBean: clientBean}
-          }).success(function(data) {
-              $scope.list();
+          ApiFactory.clientUpdate(clientBean).then(function(response) {
+              $scope.clientBeanList();
           });
-      }
+      };
 
       $scope.remove = function(clientBean) {
-          $http({
-              method : 'POST',
-              url : 'api/client/remove/' + clientBean.project
-          }).success(function(data) {
-              $scope.list();
+          ApiFactory.clientRemove(clientBean).then(function(response) {
+              $scope.clientBeanList();
           });
-      }
+      };
 
       $scope.openSchemaHTML = function(clientBean) {
           $window.open('api/client/schemahtml/' + clientBean.project);
-      }
+      };
 
       $scope.openHistoryHTML = function(clientBean) {
           $window.open('api/client/historyhtml/' + clientBean.project);
-      }
+      };
 
       $scope.task = function(clientBean, task) {
           $window.open('api/client/task/' + clientBean.project + '/' + task);
-      }
+      };
 
-      $scope.downloadEngine = function(version) {
-          $http({
-              method : 'POST',
-              url : 'api/engine/download/' + version
-          }).success(function(data) {
+      $scope.dbfluteEngine = {
+          version: null
+      };
+
+      $scope.downloadEngine = function() {
+          ApiFactory.downloadEngine($scope.dbfluteEngine).then(function(response) {
+              $scope.engineVersions();
           });
-      }
+      };
+
+      $scope.selectVersion = function(version) {
+          $scope.dbfluteEngine.version = version;
+      };
 
       $scope.setCurrentProject = function(clientBean) {
           $scope.clientBean = clientBean.clientBean;
-      }
+      };
 
       $scope.manifest();
       $scope.publicProperties();
       $scope.engineVersions();
       $scope.classification();
-      $scope.list();
+      $scope.clientBeanList();
 });
